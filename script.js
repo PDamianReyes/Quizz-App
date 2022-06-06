522586c2285b0d87be27d0abaae8400e7041ed97
PEDRO_DAMIAN_REYES_CORTEZ <pedrodamianreyescortez@gmail.com>
2022-06-05 21:28:51 -0500

    Botton, next question

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit')
 
 let currentQuiz = 0;
 
@@ -60,5 +61,9 @@ function loadQuiz() {
 
 
 
-    currentQuestion++;
 }
+
+submitBtn.addEventListener('click', () => {
+    currentQuiz++;
+    loadQuiz();
+})
